7c459166cce671db71b604cd731d84708d3666a3
Gang <gangxu79@gmail.com>
2016-05-19 16:40:11 -0400

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,36 @@
+library(dplyr)
+
+# read train data
+X_train <- read.table("./train/X_train.txt")
+Y_train <- read.table("./train/Y_train.txt")
+
+# read test data
+X_test <- read.table("./test/X_test.txt")
+Y_test <- read.table("./test/Y_test.txt")
+
+# read data description
+variable_names <- read.table("./features.txt")
+
+# read activity labels
+activity_labels <- read.table("./activity_labels.txt")
+
+# 1. Merges the training and the test sets to create one data set.
+X_total <- rbind(X_train, X_test)
+Y_total <- rbind(Y_train, Y_test)
+
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+selected_var <- variable_names[grep("mean\\(\\)|std\\(\\)",variable_names[,2]),]
+X_total <- X_total[,selected_var[,1]]
+
+# 3. Uses descriptive activity names to name the activities in the data set
+colnames(Y_total) <- "activity"
+Y_total$label <- factor(Y_total$activity, labels = as.character(activity_labels[,2]))
+
+# 4. Appropriately labels the data set with descriptive variable names.
+colnames(X_total) <- variable_names[selected_var[,1],2]
+
+# 5. From the data set in step 4, creates a second, independent tidy data set with the average
+# of each variable for each activity and each subject.
+total <- cbind(X_total, Y_total)
+total_mean <- total %>% group_by(label) %>% summarize_each(funs(mean))
+write.table(total_mean, file = "./tidydata.txt", row.names = FALSE, col.names = FALSE)
