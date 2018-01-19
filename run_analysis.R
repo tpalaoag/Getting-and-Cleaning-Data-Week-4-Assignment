9d52a7f5aa103ee7d1235d74c6e284da0a07a599
tpalaoag <33310316+tpalaoag@users.noreply.github.com>
2018-01-19 15:30:23 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,20 +1,20 @@
 library(dplyr)
 
 # read train data
-X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
-Y_train <- read.table("./UCI HAR Dataset/train/Y_train.txt")
-Sub_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+X_train <- read.table("C:/Users/CITCS/Documents/UCI HAR Dataset/UCI HAR Dataset/X_train.txt")
+Y_train <- read.table("C:/Users/CITCS/Documents/UCI HAR Dataset/train/Y_train.txt")
+Sub_train <- read.table("C:/Users/CITCS/Documents/UCI HAR Dataset/train/subject_train.txt")
 
 # read test data
-X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
-Y_test <- read.table("./UCI HAR Dataset/test/Y_test.txt")
-Sub_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+X_test <- read.table("C:/Users/CITCS/Documents/UCI HAR Dataset/test/X_test.txt")
+Y_test <- read.table("C:/Users/CITCS/Documents/UCI HAR Dataset/test/Y_test.txt")
+Sub_test <- read.table("C:/Users/CITCS/Documents/UCI HAR Datasett/test/subject_test.txt")
 
 # read data description
-variable_names <- read.table("./UCI HAR Dataset/features.txt")
+variable_names <- read.table("C:/Users/CITCS/Documents/UCI HAR Dataset/features.txt")
 
 # read activity labels
-activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
+activity_labels <- read.table("C:/Users/CITCS/Documents/UCI HAR Dataset/activity_labels.txt")
 
 # 1. Merges the training and the test sets to create one data set.
 X_total <- rbind(X_train, X_test)
@@ -38,4 +38,4 @@ colnames(X_total) <- variable_names[selected_var[,1],2]
 colnames(Sub_total) <- "subject"
 total <- cbind(X_total, activitylabel, Sub_total)
 total_mean <- total %>% group_by(activitylabel, subject) %>% summarize_each(funs(mean))
-write.table(total_mean, file = "./UCI HAR Dataset/tidydata.txt", row.names = FALSE, col.names = TRUE)
+write.table(total_mean, file = "C:/Users/CITCS/Documents/UCI HAR Dataset/tidydata.txt", row.names = FALSE, col.names = TRUE)
